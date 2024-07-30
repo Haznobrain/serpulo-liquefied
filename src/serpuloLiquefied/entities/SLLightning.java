e6c4e43d4d4f0aba892a84b509bbb72b9cfff82c
Haznobrain <143508779+Haznobrain@users.noreply.github.com>
2024-07-30 04:46:10 +0200

    yyghvgv

--- src/serpuloLiquefied/entities/SLLightning.java
+++ src/serpuloLiquefied/entities/SLLightning.java
@@ -87,6 +87,25 @@ public class CustomLightning{
             }
         }
 
-        SLFx.SLlightning.at(x, y, rotation, color, lines);
+        public Effect createLightningEffect(float stroke){
+                SLlightning = new Effect(10f, 500f, e -> {
+        if(!(e.data instanceof Seq)) return;
+        Seq<Vec2> lines = e.data();
+
+        stroke(Float.stroke * e.fout());
+        color(e.color, Color.white, e.fin());
+
+        for(int i = 0; i < lines.size - 1; i++){
+            Vec2 cur = lines.get(i);
+            Vec2 next = lines.get(i + 1);
+
+            Lines.line(cur.x, cur.y, next.x, next.y, false);
+        }
+
+        for(Vec2 p : lines){
+            Fill.circle(p.x, p.y, Lines.getStroke() / 2f);
+        }
+    }),
+}
     }
 }
